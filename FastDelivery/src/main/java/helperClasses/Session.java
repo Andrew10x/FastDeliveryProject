0be77ffc22ee2e0e48d4835f3e0d376217cd9fdb
Andrew <svt.andrey1631@gmail.com>
2023-12-25 21:40:55 +0200

    create calcPrice form, singin and singup with checking

---
+++ FastDelivery/src/main/java/helperClasses/Session.java
@@ -0,0 +1,21 @@
+package helperClasses;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class Session {
+
+    private final HttpSession session;
+
+    public Session(HttpServletRequest req) {
+        session = req.getSession(true);
+    }
+    public void createSession() {
+        int timeLive = 60 * 30;
+        session.setMaxInactiveInterval(timeLive);
+    }
+    public void closeSession() {
+        session.invalidate();
+    }
+}
+
